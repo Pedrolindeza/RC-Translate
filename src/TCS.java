441266f19ceb0153bf63486b86b5c5abd6a2294f
Bernardo <Bernardo@BERNARDO-PC>
2016-09-26 17:46:19 +0100

    :)

--- src/TCS.java
+++ src/TCS.java
@@ -3,11 +3,12 @@ import java.net.*;
 import java.util.ArrayList;
 
 class TCS{
-    public static int GN = 25;
-    public static ArrayList<TRSNode> nodes = new ArrayList<TRSNode>();
-
+    private static final int GN = 25;
+    private static ArrayList<TRSNode> nodes = new ArrayList<TRSNode>();
+    
     public static void main(String args[]) throws Exception{
         int TCSport = 58000 + GN;
+        
         if(args.length >= 2){
             for(int i = 0; i < args.length-1; i++){
                 if(args[i].equals("-p")){
@@ -23,11 +24,13 @@ class TCS{
 
         while(true){
         	java.util.Arrays.fill(receiveData, (byte) 0);
-        	java.util.Arrays.fill(sendData, (byte) 0);
         	
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
-            String sentence = new String( receivePacket.getData());
+            
+            String sentence = new String(receivePacket.getData());
+            sentence = sentence.substring(0,sentence.indexOf(0));
+            
             System.out.println("RECEIVED: " + sentence);
 
             InetAddress IPAddress = receivePacket.getAddress();
@@ -36,7 +39,7 @@ class TCS{
             String capitalizedSentence = sentence.toUpperCase();
             sendData = capitalizedSentence.getBytes();
 
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            DatagramPacket sendPacket = new DatagramPacket(sendData, capitalizedSentence.length(), IPAddress, port);
             serverSocket.send(sendPacket);
         }
     }
