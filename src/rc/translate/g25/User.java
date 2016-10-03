5b98540bb5594c5d38732e0dc3b86be11927b9c0
Bernardo Cordeiro <bernardo.f.cordeiro@tecnico.ulisboa.pt>
2016-10-03 08:20:50 +0100

    final

--- src/rc/translate/g25/User.java
+++ src/rc/translate/g25/User.java
@@ -12,29 +12,29 @@ public class User {
 	private static String TCSname = "127.0.0.1";
 	private static int TCSport = 58025;
 	private static ArrayList<String> languagesCache = null;
-	
+
 	private static ArrayList<String> getLanguages() throws IOException{
 		ArrayList<String> languages = new ArrayList<String>();
-		
+
 		String response = sendUDPMessage("ULQ" + "\n");
-		
+
 		String[] split = response.replace("\n","").split(" ");
 		if(split[0].equals("ULR")){
 			for(int i = 2; i < split.length; i++)
 				languages.add(split[i]);
 		}
-		
+
 		languagesCache = languages;
-		
+
 		return languages;
 	}
-	
+
 	private static TRSNode getTRSNode(String language) throws IOException, UNREOFException, UNRERRException{
 		String response = sendUDPMessage("UNQ " + language + "\n");
 		TRSNode node = null;
-		
+
 		String[] split = response.replace("\n","").split(" ");
-		
+
 		if(split[0].equals("UNR") && split.length == 3){
 			node = new TRSNode(language, InetAddress.getByName(split[1]), Integer.parseInt(split[2]));
 		}
@@ -44,36 +44,36 @@ public class User {
 		if(split[0].equals("UNR") && split.length > 1 && split[1].equals("ERR")){
 			throw new UNRERRException();
 		}
-		
+
 		return node;
 	}
-	
+
 	private static String sendUDPMessage(String message) throws IOException{
 		InetAddress address = InetAddress.getByName(TCSname);
-		
+
 		DatagramSocket clientSocket = new DatagramSocket();
 		byte[] sendData = new byte[message.length()];
 		byte[] receiveData = new byte[1024];
-		
+
     	java.util.Arrays.fill(receiveData, (byte) 0);
-		
+
 		sendData = message.getBytes();
-		
+
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, TCSport);
 		clientSocket.send(sendPacket);
-		
+
 		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 	    clientSocket.receive(receivePacket);
-	    
+
 	    String response = new String(receivePacket.getData());
 	    response = response.substring(0, response.indexOf(0));
-	    
+
 	    clientSocket.close();
-	    
+
 	    return response;
 	}
-	
-	public static void main(String args[]) throws InterruptedException, IOException {		
+
+	public static void main(String args[]) throws InterruptedException, IOException {
         for(int i = 0; i < args.length-1; i++){
             if(args[i].equals("-n")){
                 TCSname = args[i+1];
@@ -82,14 +82,14 @@ public class User {
                 TCSport = Integer.parseInt(args[i+1]);
             }
         }
-        
+
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-        
+
         while(true){
         	String input = stdin.readLine();
-        	String[] split = input.split(" ");
-        	
-        	if(split[0].equals("list")){        		
+        	final String[] split = input.split(" ");
+
+        	if(split[0].equals("list")){
         		Thread thread = new Thread(new Runnable() {
         		    public void run() {
 						try {
@@ -103,15 +103,15 @@ public class User {
 						}
         		    }
         		});
-        		
+
         		thread.start();
         		thread.join(3000);
-        		
+
         		if(!thread.getState().equals(Thread.State.TERMINATED)){
         			thread.interrupt();
             		System.out.println("LIST: timeout");
         		}
-        		
+
         	}
         	else if(split[0].equals("request")){
         		if(split.length < 4){
@@ -122,7 +122,7 @@ public class User {
         		}
         		else{
             		Thread thread = new Thread(new Runnable() {
-            			
+
             		    public void run() {
             		    	try {
             		    		String language = languagesCache.get(Integer.parseInt(split[1])-1);
@@ -145,10 +145,10 @@ public class User {
 							}
             		    }
             		});
-            		
+
             		thread.start();
             		thread.join(3000);
-            		
+
             		if(!thread.getState().equals(Thread.State.TERMINATED)){
             			thread.interrupt();
                 		System.out.println("REQUEST: timeout");
@@ -162,7 +162,7 @@ public class User {
         		System.out.println("Unknown command");
         	}
         }
-        
+
         return;
 	}
 }
